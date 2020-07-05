d441df340bdc1095dcd17ecbb838b3ec054ae716
zmw <zmwwork@163.com>
2020-07-05 21:56:11 +0800

    types 2

---
+++ src/jass/lib/Commonj.ts
@@ -0,0 +1,5 @@
+import { Jass_Type } from "./types";
+
+export declare function Jass_Constant_Native_ConvertRace(i:Jass_Type.Jass_Integer):Jass_Type.Jass_Race;
+export declare function Jass_Constant_NativeCOnvertAllianceType(i:Jass_Type.Jass_Integer):Jass_Type.Jass_AllianceType;
+export declare function Jass_Constant_ConvertRacePref(i:Jass_Type.Jass_Integer):Jass_Type.Jass_RacePreference;
